f53e9d57fb6f8e5e198b3c00c834c031c120ca29
pyongwonlee <pyongwonlee@hotmail.com>
2018-04-18 20:12:27 -0400

    table fix

--- src/components/contact/ContactListRow.js
+++ src/components/contact/ContactListRow.js
@@ -6,6 +6,7 @@ const ContactListRow = ({contact}) => {
   console.log('contact', contact);
   return (
     <tr>
+      <td>&nbsp;</td>
       <td><Link to={'/contact/' + contact.id}>{contact.name}</Link></td>
       <td>{contact.email}</td>
     </tr>
